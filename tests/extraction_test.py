8d5ac62756d77ca931e140191d864961c2880c17
steeven-louk <steevenlouk.sl@gmail.com>
2024-04-15 23:32:32 +0200

    mise en place des tests d'extraction

--- tests/extraction_test.py
+++ tests/extraction_test.py
@@ -1,9 +1,43 @@
 import unittest
 
+from extraction.extraction import extract_csv, extract_json, extract_from_xml, extract_from_api, extract_from_database
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+class TestExtraction(unittest.TestCase):
+    def test_extract_csv(self):
+        source_config = '../data/client1.csv'
+        data = extract_csv(source_config)
+        # Vérifier si les données extraites du fichier CSV sont correctes
+
+    def test_extract_json(self):
+        source_config = '../data/users.json'
+
+        data = extract_json(source_config)
+        # Vérifier si les données extraites du fichier JSON sont correctes
+
+    def test_extract_xml(self):
+        data = '../data/book.xml'
+        data = extract_from_xml(data)
+        # Vérifier si les données extraites du fichier XML sont correctes
+
+    def test_extract_api(self):
+        data = 'https://jsonplaceholder.typicode.com/todos'
+        extract_from_api(data)
+        # Vérifier si les données extraites de l'API sont correctes
+
+ #   def test_extract_database(self):
+#        source_config = {
+#            'type': 'database',
+#            'connection_params': {
+#                'host': 'localhost',
+#                'user': 'root',
+#                'password': '',
+#                'database': 'dm22'
+#            },
+#            'query': 'SELECT * FROM membres'
+#        }
+#        data = extract_from_database(source_config)
+        # Vérifier si les données extraites de la base de données sont correctes
 
 
 if __name__ == '__main__':
---
+++ tests/transformation_test.py
@@ -0,0 +1,29 @@
+import unittest
+
+from transformation.transformation import merge_data, handle_missing_values
+
+
+class TestTransformation(unittest.TestCase):
+    def test_merge_data(self):
+        source_data = {'identifiant': [1, 2, 3], 'nom': ['Alice', 'Bob', 'Charlie']}
+        with_data = {'identifiant': [1, 2, 3], 'email': ['alice@example.com', 'bob@example.com', 'charlie@example.com']}
+        common_column = 'identifiant'
+        merged_data = merge_data('merge_data', source_data)
+        # Vérifier si les données fusionnées sont correctes
+
+    def test_handle_missing_values(self):
+        source_data = {'identifiant': [1, 2, None], 'nom': ['Alice', None, 'Charlie']}
+        cleaned_data = handle_missing_values(source_data)
+        return cleaned_data
+        # Vérifier si les valeurs manquantes ont été correctement traitées
+
+    def test_filter(self):
+        source_data = {'identifiant': [1, 2, 3], 'genre': ['M', 'F', 'M']}
+        condition = 'genre == "M"'
+        filtered_data = apply_transformation('filter', source_data, condition)
+        # Vérifier si les données ont été correctement filtrées selon la condition
+
+    # Ajoutez d'autres méthodes de test pour les autres transformations...
+
+if __name__ == '__main__':
+    unittest.main()
